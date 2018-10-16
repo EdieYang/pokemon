8d86d7567f9ed1b21af62106e6ca2be34069d0b7
zhouwandou1991 <791029802@qq.com>
2018-10-16 13:03:42 +0800

    完善微信支付回调逻辑 user_record新增city字段

--- src/main/java/com/pokepet/service/impl/OrderServiceImpl.java
+++ src/main/java/com/pokepet/service/impl/OrderServiceImpl.java
@@ -3,18 +3,20 @@ package com.pokepet.service.impl;
 import java.util.List;
 import java.util.Map;
 
-import com.pokepet.model.User;
-import com.pokepet.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.pokepet.dao.CommodityMapper;
 import com.pokepet.dao.OrderMallMapper;
+import com.pokepet.model.Commodity;
 import com.pokepet.model.OrderMall;
+import com.pokepet.model.User;
 import com.pokepet.service.IOrderService;
-import org.springframework.transaction.annotation.Transactional;
+import com.pokepet.service.IUserService;
 
 /**
  * Created by Fade on 2018/8/22.
@@ -23,17 +25,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class OrderServiceImpl implements IOrderService {
 
-
-    @Autowired
-    private OrderMallMapper orderMallMapper;
+	@Autowired
+	private OrderMallMapper orderMallMapper;
 
 	@Autowired
 	private IUserService userService;
+	
+	@Autowired
+	private CommodityMapper commodityMapper;
 
-    @Override
-    public List<OrderMall> getOrderListByUserId(String userId) {
-        return null;
-    }
+	@Override
+	public List<OrderMall> getOrderListByUserId(String userId) {
+		return null;
+	}
 
 	@Override
 	public JSONObject selectOrderListByUserId(String userId, int pageNum, int pageSize) {
@@ -60,38 +64,48 @@ public class OrderServiceImpl implements IOrderService {
 	}
 
 	@Override
-    public List<OrderMall> getOrderListByParameter(Map<String, Object> param) {
-        return null;
-    }
+	public List<OrderMall> getOrderListByParameter(Map<String, Object> param) {
+		return null;
+	}
 
-    @Override
-    public void createOrder(OrderMall orderPay) {
-    	orderMallMapper.insertSelective(orderPay);
-    }
+	@Override
+	public void createOrder(OrderMall orderPay) {
+		orderMallMapper.insertSelective(orderPay);
+	}
 
-    @Override
-    public boolean updateOrder(OrderMall orderPay) {
-		return orderMallMapper.updateByPrimaryKeySelective(orderPay)>0;
-    }
+	@Override
+	public boolean updateOrder(OrderMall orderPay) {
+		return orderMallMapper.updateByPrimaryKeySelective(orderPay) > 0;
+	}
 
 	@Transactional
 	@Override
-	public boolean settleAccounts(OrderMall orderMall,String outTradeNo) {
-		boolean uptResult=updateOrder(orderMall);
-		if(uptResult){
-			OrderMall orderOrigin=getOrder(outTradeNo);
+	public boolean settleAccounts(OrderMall orderMall, String outTradeNo) {
+		boolean FLAG = false;
+		try {
+			FLAG = updateOrder(orderMall);
+			if (FLAG) {
+				OrderMall orderOrigin = getOrder(outTradeNo);
 
-			//扣除金币(未判断金币数量是否足够扣除,前端已做过校验)
-			if(orderOrigin.getBuyType().equals("1")){
-				String userId=orderOrigin.getUserId();
-				User user=userService.getUserInfo(userId);
-				int leftCoin=user.getChipCount()-orderOrigin.getCoin();
-				user.setUserId(userId);
-				user.setChipCount(leftCoin);
-				return userService.modifyUser(user)>0;
+				// 扣除金币(未判断金币数量是否足够扣除,前端已做过校验)
+				if (orderOrigin.getBuyType().equals("1")) {
+					String userId = orderOrigin.getUserId();
+					User user = userService.getUserInfo(userId);
+					int leftCoin = user.getChipCount() - orderOrigin.getCoin();
+					user.setUserId(userId);
+					user.setChipCount(leftCoin);
+					userService.modifyUser(user);
+				}
+				
+				//扣除库存
+				Commodity commodity = commodityMapper.selectByPrimaryKey(orderOrigin.getCommodityId());
+				commodity.setInventory(commodity.getInventory()-1);
+				commodityMapper.updateByPrimaryKeySelective(commodity);
 			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		return false;
+		return FLAG;
 	}
 
 	@Override
@@ -119,22 +133,22 @@ public class OrderServiceImpl implements IOrderService {
 	@Override
 	public JSONObject checkBuyStatusByUserId(String userId, String commodityId, int dayRange) {
 		JSONObject result = new JSONObject();
-		//获取最近一条有效的未完成订单
+		// 获取最近一条有效的未完成订单
 		OrderMall unfilledOrder = orderMallMapper.selectLastUnfilledOrderByUserId(userId);
-		if(null != unfilledOrder){
+		if (null != unfilledOrder) {
 			result.put("FLAG", false);
 			result.put("msg", "您有未完成订单，请完成后继续兑换。");
 			return result;
 		}
-		
-		//获取一周内该用户对该商品的有效订单
+
+		// 获取一周内该用户对该商品的有效订单
 		OrderMall commodityOrder = orderMallMapper.selectLastCommodityOrderByUserId(userId, commodityId, dayRange);
-		if(null != commodityOrder){
+		if (null != commodityOrder) {
 			result.put("FLAG", false);
-			result.put("msg", "该物品"+dayRange+"天只能兑换一次哦，请过段时间后继续兑换。");
+			result.put("msg", "该物品" + dayRange + "天只能兑换一次哦，请过段时间后继续兑换。");
 			return result;
 		}
-		
+
 		result.put("FLAG", true);
 		return result;
 	}
