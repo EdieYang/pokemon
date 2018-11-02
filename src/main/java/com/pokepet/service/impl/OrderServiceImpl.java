71548b6a7773b8e4a22d52c105c28111fd789395
zhouwandou1991 <791029802@qq.com>
2018-11-02 15:23:59 +0800

    fix 商城中商品库存bug

--- src/main/java/com/pokepet/service/impl/OrderServiceImpl.java
+++ src/main/java/com/pokepet/service/impl/OrderServiceImpl.java
@@ -3,10 +3,6 @@ package com.pokepet.service.impl;
 import java.util.List;
 import java.util.Map;
 
-import com.pokepet.model.User;
-import com.pokepet.service.IUserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
